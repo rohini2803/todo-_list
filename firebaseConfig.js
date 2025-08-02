60fff7dbb96a40f1c247170e094d5d56e5fd8de4
rohini2803 <rohiniselvaraj2005@gmail.com>
2025-08-02 16:28:00 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,27 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-
-
-// Your Firebase configuration object
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA8BykUfTs3iYtBf5k-LjcF5D0-1beeULY",
-  authDomain: "react-todo-7e88a.firebaseapp.com",
-  projectId: "react-todo-7e88a",
-  storageBucket: "react-todo-7e88a.firebasestorage.app",
-  messagingSenderId: "655556786906",
-  appId: "1:655556786906:web:2decc5571139f9691143e3",
-  measurementId: "G-GR9XWYKGZG"
+  apiKey: "AIzaSyA7gFenIHHNW-0eq1ouSY814EhJhxC5F8c",
+  authDomain: "todo-list-c29a5.firebaseapp.com",
+  projectId: "todo-list-c29a5",
+  storageBucket: "todo-list-c29a5.firebasestorage.app",
+  messagingSenderId: "583816789156",
+  appId: "1:583816789156:web:22ffc450206db61453ba31",
+  measurementId: "G-YSSXRVZXQ6"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Auth with persistence
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-const db = getFirestore(app);
-
-export { db, auth };
